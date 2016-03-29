f25d1704485bc3b88799cb8a00749a9375ca493e
marco-t <tobine22@hotmail.com>
2016-03-29 10:01:56 -0230

    Created line_items model

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,6 @@
 class Product < ActiveRecord::Base
+  has_many :line_items
+  before_destroy :ensure_not_referenced_by_any_line_item
   validates :title, :description, :image_url, presence: true
   validates :title, uniqueness: true, length: { 
     minimum: 10,
@@ -14,4 +16,14 @@ class Product < ActiveRecord::Base
   def self.latest
     Product.order(:updated_at).last
   end
+
+  private
+    def ensure_not_referenced_by_any_line_item
+      if line_items.empty?
+        return true
+      else
+        errors.add(:base, 'Line Items present')
+        return false
+      end
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
+  # as: 'store' creates a store_path accessor method.
+  root 'store#index', as: 'store'
+  
+  resources :line_items
+  resources :carts
   get 'store/index'
   resources :products
   
-  # as: 'store' creates a store_path accessor method.
-  root 'store#index', as: 'store'
 end
