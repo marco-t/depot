1732b18374be11d79778342e10b287d46a872806
marco-t <tobine22@hotmail.com>
2016-03-30 13:59:35 -0230

    who_bought atom feed

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
   resources :orders
-  # as: 'store' creates a store_path accessor method.
-  root 'store#index', as: 'store'
-  
   resources :line_items
   resources :carts
+
   get 'store/index'
-  resources :products
+  resources :products do
+    get :who_bought, on: :member
+  end
   
+  # as: 'store' creates a store_path accessor method.
+  root 'store#index', as: 'store'
 end
